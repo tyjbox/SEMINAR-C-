3d72b1ea05b3a31627c5a044798f8a08621c2b98
alexander <tyjbox@gmail.com>
2022-06-05 04:21:00 +0700

    Add task 10,13,15

--- Program.cs
+++ Program.cs
@@ -66,3 +66,60 @@ for (int i=0; i<50; i++)
     num = (-(~num)); num = (-(~num));
 }
 */
+
+// Задача 10.  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+void ShowNunber()
+{
+int num = new Random().Next(100, 999);
+Console.Write(num + " -> ");
+int num1 = num / 10;
+int n = num1 % 10;
+
+Console.Write(n);
+}
+ShowNunber();
+*/
+
+/*
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void ShowNunber2(int num)
+
+{
+    if (num  > 99 )
+    {
+        int num1 = num % 10;
+        Console.Write(num1 + "third number");
+        
+    }
+    else 
+    {
+        Console.Write("No third number");
+    }
+    }
+
+Console.Write("Imput number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+ShowNunber2(num);
+*/
+
+/*
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void SowDayOff(int num)
+{
+    if(num <= 7 && num >= 6)
+    {
+        Console.Write("It's a day off");
+    }
+    else
+    {
+        Console.Write("It's not a day off");
+    }
+}
+
+Console.Write("Imput day number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+SowDayOff(num);
+*/
