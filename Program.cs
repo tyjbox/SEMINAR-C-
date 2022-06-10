612af9232108967a5fe74603344cd6bfc7860d02
alexander <tyjbox@gmail.com>
2022-06-11 01:59:54 +0700

    Add task 19, 21, 23

--- Program.cs
+++ Program.cs
@@ -123,3 +123,77 @@ Console.Write("Imput day number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 SowDayOff(num);
 */
+
+// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+/*
+Console.Write("Enter your number: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Your number is: {number} - polindrom.");
+  }
+  else Console.WriteLine($"Your number is: {number} - not polindrom.");
+}
+
+if (number!.Length == 5)
+{
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Enter the correct number  ");
+
+*/
+
+// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+/*
+int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Enter your coordinate {coorName} point {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double x1, double x2, double y1, double y2, double z1, double z2)
+{
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Segment length  {segmentLength}");
+
+*/
+
+// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+/*
+Console.Clear();
+void Cube(int N)
+{
+    int count = 1;
+    Console.Write(N + " -> ");
+    while(count <= N)
+    {
+        Console.Write(count * count * count + " ");
+        count++;
+    }
+}
+
+Console.WriteLine("Enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 0)
+    Cube(num);
+else
+    Console.Write("Enter positiv number");    
+
+*/
